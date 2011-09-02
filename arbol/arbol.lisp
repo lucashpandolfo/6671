c5b0b639f6167478f393dab5ce18646d021ba063
Lucas <lucashpandolfo@gmail.com>
2011-09-01 21:50:11 -0300

    Tree demo reworked. Tree levels can be modified by m and M keys.

--- arbol/arbol.lisp
+++ arbol/arbol.lisp
@@ -1,32 +1,59 @@
 (require 'sistemas-graficos)
 
+(defclass ventana-arbol (sg:framework-window)
+  ((edad :initform 4 :accessor edad)))
+
+(defmethod aumentar-edad ((ventana ventana-arbol))
+  (when (< (edad ventana) 6)
+    (incf (edad ventana))))
+
+(defmethod disminuir-edad ((ventana ventana-arbol))
+  (when (> (edad ventana) 0)
+    (decf (edad ventana))))
+
+(defmethod initialize-instance :after ((ventana ventana-arbol) &key)
+  (sg:add-key ventana #\m #'aumentar-edad)
+  (sg:add-key ventana #\M #'disminuir-edad))
+
 (defun rama (ancho largo)
-  (gl:color 0.6 0.3 0)
-  (glu::cylinder (glu:new-quadric) ancho ancho largo 20 1))
+  (gl:color 0.5 0.2 0)
+  (gl:with-pushed-matrix
+    (gl:translate 0 0 (/ largo 2))
+    (glu::cylinder (glu:new-quadric) ancho (* 0.7 ancho) largo 20 1)))
 
 (defun hoja (largo)
-  (gl:color 0 0.7 0.2)
+  (gl:color 0 0.5 0.1)
   (gl:with-primitive :quads
     (gl:vertex 0 (/ (- largo) 2) 0)
     (gl:vertex 0 0 (- largo))
     (gl:vertex 0 (/ largo 2) 0)
     (gl:vertex 0 0 largo)))
 
-(defun arbol (n)
-  (if (eq n 0)
-      (hoja 4)
-      (progn
-	(rama 1 6)
-	(gl:translate 0 0 6)
+(defun arbol (n &optional (nivel 0))
+  (let ((ancho-rama (* 1 (expt 0.5 nivel)))
+	(largo-rama (* 6 (expt 0.6 nivel)))
+	(largo-hoja (* 4 (expt 0.7 nivel)))
+	(angulos '(50 60 70 65 45)))
+    (if (eq n 0)
+	(hoja largo-hoja)
+	(progn
+	  (gl:with-pushed-matrix
+	    (gl:translate 0 0 (- (/ largo-rama 2)))
+	    (rama ancho-rama  largo-rama))
 	(dotimes (i 5)
 	  (gl:with-pushed-matrix
-	    (gl:rotate (* (/ 360 5) i) 0 0 1)
-	    (gl:rotate 50 0 1 0)
-	    (gl:scale 0.7 0.7 0.7)
-	    (arbol (- n 1)))))))
-      
-(defmethod sg:draw ((window sg:framework-window))
-  (gl:color 0.1 0.3 0.7)
-  (arbol 5))
+	    (gl:translate 0 0 (if (eq n 1)
+				  largo-rama
+				  (* 0.9 largo-rama)))
 
-(sg:main (make-instance 'sg:framework-window))
+	    (gl:rotate (* (/ 360 5) i) 0 0 1)
+	    (when (eq n 1)
+	      (gl:translate (/ largo-hoja 2) 0 0))
+	    (gl:rotate (nth (mod (+ i nivel) 5) angulos) 0 1 0)
+	    (arbol (- n 1) (+ nivel 1))))))))
+
+(defmethod sg:draw ((window ventana-arbol))
+  (arbol (edad window)))
+
+(sg:main (make-instance 'ventana-arbol))
+
