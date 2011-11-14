6e5ce691ab10e99377366f27b82b106c88ca9ef5
Lucas <lucashpandolfo@gmail.com>
2011-11-14 16:02:59 -0300

    Uncommited old irrelevant changes

--- arbol/arbol.lisp
+++ arbol/arbol.lisp
@@ -4,22 +4,25 @@
   ((edad :initform 4 :accessor edad)
    (lista :initform nil :accessor lista)
    (usar-lista :initform nil :accessor usar-lista)
-   (capturar-mouse :initform nil :accessor capturar-mouse)))
+   (porcentaje :initform 0 :accessor porcentaje)
+   (capturar-mouse :initform nil :accessor capturar-mouse)
+   (posicion-anterior :initform (make-array 3) :accessor posicion-anterior)))
 
 (defun rama (ancho largo)
-  (gl:color 0.74 0.33 0.18)
+  (gl:color 0.74 0.2 0.1)
   (gl:with-pushed-matrix
     (gl:translate 0 0 (/ largo 2))
     (glu::cylinder (glu:new-quadric) ancho (* 0.7 ancho) largo 20 1)))
 
 (defun hoja (largo)
   (gl:color 0 0.5 0.1 0.5)
-; (gl:color 1 0.51 0.7)
   (gl:with-primitive :quads
+    (gl:normal -1 0 0)
     (gl:vertex 0 (/ (- largo) 2) 0)
     (gl:vertex 0 0 (- largo))
     (gl:vertex 0 (/ largo 2) 0)
     (gl:vertex 0 0 largo)))
+
 
 (defun arbol (n &optional (nivel 0))
   (let ((ancho-rama (* 1 (expt 0.5 nivel)))
@@ -44,10 +47,35 @@
 	    (gl:rotate (nth (mod (+ i nivel) 5) angulos) 0 1 0)
 	    (arbol (- n 1) (+ nivel 1))))))))
 
+(defun arbol-animado (n porcentaje &optional (nivel 0))
+  (gl:with-pushed-matrix
+    (gl:scale porcentaje porcentaje porcentaje)
+    (let ((ancho-rama (* 1 (expt 0.5 nivel)))
+	  (largo-rama (* 6 (expt 0.6 nivel)))
+	  (largo-hoja (* 4 (expt 0.7 nivel)))
+	  (angulos '(50 60 70 65 45)))
+      (if (eq n 0)
+	  (hoja largo-hoja)
+	  (progn
+	    (gl:with-pushed-matrix
+	      (gl:translate 0 0 (- (/ largo-rama 2)))
+	      (rama ancho-rama  largo-rama))
+	    (dotimes (i 5)
+	      (gl:with-pushed-matrix
+		(gl:translate 0 0 (if (eq n 1)
+				      largo-rama
+				      (* 0.9 largo-rama)))
+
+		(gl:rotate (* (/ 360 5) i) 0 0 1)
+		(when (eq n 1)
+		  (gl:translate (/ largo-hoja 2) 0 0))
+		(gl:rotate (nth (mod (+ i nivel) 5) angulos) 0 1 0)
+		(arbol-animado (- n 1) porcentaje (+ nivel 1)))))))))  
+
 (defmethod toggle-lista ((ventana ventana-arbol))
   (setf (usar-lista ventana) (not (usar-lista ventana)))
   (recompilar-lista ventana)
-  (format t "Usra lista compilada: ~a~%" (usar-lista ventana)))
+  (format t "Usar lista compilada: ~a~%" (usar-lista ventana)))
 
 (defmethod toggle-capturar-mouse ((ventana ventana-arbol))
   (if (capturar-mouse ventana)
@@ -59,6 +87,8 @@
 	(with-accessors ((width glut:width) (height glut:height)) ventana
 	  (glut:warp-pointer (floor (/ width 2))
 			     (floor (/ height 2))))
+	(setf (posicion-anterior ventana) #((floor (/ width 2))
+					    (floor (/ height 2)) 0))
 	(setf (capturar-mouse ventana) t)))
   (format t "Capturar mouse: ~a~%" (capturar-mouse ventana)))
 
@@ -77,10 +107,19 @@
     (recompilar-lista ventana)))
 
 (defmethod initialize-instance :after ((ventana ventana-arbol) &key)
+  (sg:add-key ventana #\r (lambda (ventana) 
+			    (setf (porcentaje ventana) 0)))
   (sg:add-key ventana #\c #'toggle-capturar-mouse)
   (sg:add-key ventana #\m #'aumentar-edad)
   (sg:add-key ventana #\M #'disminuir-edad)
   (sg:add-key ventana #\l #'toggle-lista))
+
+(defmethod rotar-mundo ((ventana ventana-arbol) x y z)
+  (let ((anterior (sg::world-rotation ventana)))
+    (setf (sg::world-rotation ventana) 
+	  (coerce 
+	   (mapcar #'+ (coerce anterior 'list) (list x y z))
+	   'vector))))
 
 (defmethod sg:draw ((ventana ventana-arbol))
   (gl:enable :blend)
@@ -89,17 +128,23 @@
   (unless (lista ventana)
     (setf (lista ventana) (gl:gen-lists 1)))
 
-  (if (usar-lista ventana)
-      (gl:call-list (lista ventana))
-      (arbol (edad ventana))))
+  (when (< (porcentaje ventana) 1)
+    (glut:schedule-timer 50 (lambda () 
+			       (setf (porcentaje ventana) 
+				     (+ (porcentaje ventana) 0.01))
+			       (glut:post-redisplay))))
+
+  (arbol-animado (edad ventana) (porcentaje ventana)))
 
 (defmethod sg::mouse-passive-motion ((ventana ventana-arbol) x y)
   (when (capturar-mouse ventana)
     (with-accessors ((width glut:width) (height glut:height)) ventana
-      (when (or
-	     (> (abs (- (/ width 2) x)) (/ width 4))
-	     (> (abs (- (/ height 2) y)) (/ height 4)))
-	(glut:warp-pointer (floor (/ width 2))
-			   (floor (/ height 2)))))))
+      (if (or
+	   (> (abs (- (/ width 2) x)) (/ width 4))
+	   (> (abs (- (/ height 2) y)) (/ height 4)))
+	  (glut:warp-pointer (floor (/ width 2))
+			     (floor (/ height 2)))))))
+    
+  
 
 (sg:main (make-instance 'ventana-arbol))
--- primitivas.lisp
+++ primitivas.lisp
@@ -1,12 +1,14 @@
-(in-package #:sistemas-graficos)
-
 (defun cuadrado ()
   "Dibuja un cuadrado de lado 1 centrado."
   (gl:with-primitive :triangle-fan
     (gl:normal 0 0 -1)
+    (gl:tex-coord 0 0)
     (gl:vertex -0.5 -0.5)
+    (gl:tex-coord 1 0)
     (gl:vertex -0.5 0.5)
+    (gl:tex-coord 1 1)
     (gl:vertex 0.5 0.5)
+    (gl:tex-coord 0 1)
     (gl:vertex 0.5 -0.5)))
 
 (defun circulo (segmentos)
@@ -21,8 +23,7 @@ segmentos dados."
 	 (gl:vertex (cos i) (sin i) 0))))
 
 (defun cilindro (lados &optional (radio-superior 1))
-  "Dibuja un cilindro de radio 1 y altura 1 centrado, a lo largo del
-eje z."
+  "Dibuja un cilindro de radio 1 y altura 1."
   (gl:with-primitive :triangle-strip
     (loop for i = 0 then (+ i (/ (* 2 pi) lados))
        while (<= i (* 2 pi))
